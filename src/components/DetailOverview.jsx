de1d0fd235e0f34b8e160e17771247181ef93fe6
chitrapg <kishanurankar@gmail.com>
2024-11-02 18:42:15 +0530

    All commit

--- src/components/DetailOverview.jsx
+++ src/components/DetailOverview.jsx
@@ -1,15 +1,32 @@
-import React from "react";
-// import "./DisplaySearch.css";
-// import { FaStar } from "react-icons/fa";
+import React, { useEffect } from "react";
 import "./DetailOverview.css";
 
 const DetailOverview = ({ data }) => {
-  //   console.log(Rating);
   console.log(data);
+
+  useEffect(() => {
+    // Store data in localStorage when it changes
+    if (data) {
+      localStorage.setItem("movieData", JSON.stringify(data));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    // Check localStorage for existing data on component mount
+    const storedData = localStorage.getItem("movieData");
+    if (storedData) {
+      // If data exists, parse it and set it to the component's state or props
+      // You may need to handle this in the parent component
+      // Here we log it to verify it's working
+      console.log("Loaded data from localStorage:", JSON.parse(storedData));
+    }
+    console.log(storedData)
+  }, []);
+
+  
 
   return (
     <div className="contain">
-
       <h1>Overview of movie</h1>
       <div className="detail_container">
         <div className="detail_display">
@@ -26,49 +43,40 @@ const DetailOverview = ({ data }) => {
             <p>{data.Plot}</p>
           </div>
           <div className="rating_container">
-            {data.Ratings.map((m) => {
-              return (
-                <span key={m.Source}>
-                  {m.Source} - {m.Value}
-                </span>
-              );
-            })}
+            {data.Ratings.map((m) => (
+              <span key={m.Source}>
+                {m.Source} - {m.Value}
+              </span>
+            ))}
           </div>
-
           <div className="director_container">
             <h3>
               Director{" "}
-              {data.Director.split(" ").map((director) => {
-                return (
-                  <a href={director} key={director}>
-                    {director}
-                  </a>
-                );
-              })}
+              {data.Director.split(" ").map((director) => (
+                <a href={director} key={director}>
+                  {director}
+                </a>
+              ))}
             </h3>
           </div>
           <div className="stars_container">
             <h3>
               Stars{" "}
-              {data.Actors.split("").map((actor, index) => {
-                return (
-                  <a href={actor} key={index}>
-                    {actor}
-                  </a>
-                );
-              })}
+              {data.Actors.split(", ").map((actor, index) => (
+                <a href={actor} key={index}>
+                  {actor}
+                </a>
+              ))}
             </h3>
           </div>
           <div className="writers_container">
             <h3>
               Writers{" "}
-              {data.Writer.split("").map((writer) => {
-                return (
-                  <a href={writer} key={writer}>
-                    {writer}
-                  </a>
-                );
-              })}
+              {data.Writer.split(", ").map((writer) => (
+                <a href={writer} key={writer}>
+                  {writer}
+                </a>
+              ))}
             </h3>
           </div>
         </div>
